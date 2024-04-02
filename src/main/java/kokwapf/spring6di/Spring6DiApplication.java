236d93066a6b857181b1063258b8ef5c1cb7306b
Kkwafo <kkwafoawua@gmail.com>
2024-04-02 15:50:37 -0300

    Initial project

---
+++ src/main/java/kokwapf/spring6di/Spring6DiApplication.java
@@ -0,0 +1,20 @@
+package kokwapf.spring6di;
+
+import kokwapf.spring6di.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+        MyController controller = ctx.getBean(MyController.class);
+        System.out.println("Main class");
+        System.out.println(controller.sayHello());
+    }
+
+}
